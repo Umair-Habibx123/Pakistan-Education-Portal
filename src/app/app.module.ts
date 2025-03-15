a3c8fda4d0bf647479f5ba9d8b5d26c810b5b9ae
Umair Habib <125673086+Umair-Habibx123@users.noreply.github.com>
2025-03-15 18:31:37 +0500

    angular animation added package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,6 +42,7 @@ import { AboutUs2Component } from '../../libs/pages/about-us-page/about-us2/abou
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    BrowserAnimationsModule, // angular Animation Module... 
   ],
   providers: [],
   bootstrap: [AppComponent]
