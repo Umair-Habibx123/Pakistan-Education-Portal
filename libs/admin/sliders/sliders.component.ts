46007b8289d9c7c258f1e54971bc301ccd054fc4
M.LUQMAN LUQMAN <mluqman9154651@gmail.com>
2025-03-26 11:13:08 +0500

    uploadimage

--- libs/admin/sliders/sliders.component.ts
+++ libs/admin/sliders/sliders.component.ts
@@ -1,5 +1,26 @@
-import { Component } from '@angular/core';
+// // import { Component } from '@angular/core';
 
+
+// // @Component({
+// //   selector: 'app-sliders',
+// //   templateUrl: './sliders.component.html',
+// //   styleUrls: ['./sliders.component.scss']
+// // })
+// // export class SlidersComponent {
+  
+// //   isModalOpen = false;
+
+// //   slides = [
+// //     { logo: '../../../assets/slider.png' },
+// //     { logo: '../../../assets/slider.png' },
+// //     { logo: '../../../assets/slider.png' },
+// //     { logo: '../../../assets/slider.png' },
+// //   ];
+
+
+// }
+
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sliders',
@@ -17,5 +38,17 @@ export class SlidersComponent {
     { logo: '../../../assets/slider.png' },
   ];
 
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
 
+  triggerFileUpload() {
+    this.fileInput.nativeElement.click(); // Open file dialog
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      console.log('Selected file:', file);
+    }
+  }
 }
